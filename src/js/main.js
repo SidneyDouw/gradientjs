3e42cda3a47fe4640fca40ae3fff2cbb9a0c6887
Sidney Douw <sidneydouw@sidneys-mbp.speedport_w_724v_09011603_05_010>
2017-07-03 04:43:24 +0200

    cleanup

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,5 @@
 require(['Gradient'], function(Gradient) {
 
-	var myDiv1 = document.getElementById('testDiv1');
-	var myDiv2 = document.getElementById('testDiv2');
-
-	var gradient1 = new Gradient(myDiv1, 4);
-	var gradient2 = new Gradient(myDiv2, 4);
-
 	return Gradient;
 
 });
